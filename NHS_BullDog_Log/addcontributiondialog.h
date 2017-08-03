c455f10d95bcad4f2a672a82e83b13b4ca66d5b5
shanetorres <stor314@gmail.com>
2017-08-03 14:58:43 -0400

    Several bug fixes - fixed add dialog close button not re-enabling buttons - fixed index out of bounds error when a second student's event is added - added ascii value detection for validation test in populateCurrentStudentsModel

--- NHS_BullDog_Log/addcontributiondialog.h
+++ NHS_BullDog_Log/addcontributiondialog.h
@@ -14,6 +14,7 @@ class AddContributionDialog : public QDialog
 public:
     explicit AddContributionDialog(QWidget *parent = 0);
     ~AddContributionDialog();
+    void AddContributionDialog::reject();
 
 private slots:
     void on_okButton_clicked();
--- NHS_BullDog_Log/currentstudent.cpp
+++ NHS_BullDog_Log/currentstudent.cpp
@@ -17,31 +17,24 @@ CurrentStudent::CurrentStudent(QString first, QString last, int cont, int serv, 
 }
 
 void CurrentStudent::setFirstName(QString first) { firstName = first; }
-
 QString CurrentStudent::getFirstName() { return firstName; }
 
 void CurrentStudent::setLastName(QString last) { lastName = last; }
-
 QString CurrentStudent::getLastName() { return lastName; }
 
 void CurrentStudent::setContributions(int cont) { contributions = cont; }
-
 int CurrentStudent::getContributions() { return contributions; }
 
 void CurrentStudent::setServProjects(int serv) { servProjects = serv; }
-
 int CurrentStudent::getServProjects() { return servProjects; }
 
 void CurrentStudent::setAttendedMeetings(int meetings) { attendedMeetings = meetings; }
-
 int CurrentStudent:: getAttendedMeetings() { return attendedMeetings; }
 
 void CurrentStudent::setInductionAttendance(bool induct) { inductionAttend = induct; }
-
 bool CurrentStudent::getInductionAttendance() { return inductionAttend; }
 
 void CurrentStudent::setStudentEvent(QString event) { studentEvents.push_back(event); }
-
 QString CurrentStudent::getStudentEvent(int i) { return studentEvents[i]; }
 
 void CurrentStudent::setEventVector(QVector<QString> events)
